07ffe243ff347f30c0415819e56c4f79b0fe9ae9
Shirin <shirin.monzavi.1372@gmail.com>
2025-08-11 18:40:40 +0330

    remove cooment

--- ProxyDesignPatternSample/Program.cs
+++ ProxyDesignPatternSample/Program.cs
@@ -2,12 +2,10 @@
 
 DocumentEditor document = new();
 
-// Insert image proxies - real images not loaded yet
 document.Add(new ImageProxy("big_photo.jpg"));
 document.Add(new ImageProxy("another_photo.png"));
 
 Console.WriteLine("Document created with image proxies (no real images loaded yet).");
 
 document.DrawAll();
-
 document.DrawAll();
